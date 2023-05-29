a3c6cbb8727e0f7ee00867565156328ecd6a9c8e
raceychan <raceychan@gmail.com>
2023-05-30 03:20:19 +0800

    lazyvim

--- packages/neovim.py
+++ packages/neovim.py
@@ -1,18 +1,40 @@
-from base import git_clone, cmd
+from base import git_clone, shell
 
 # NOTE: since this is a configurable package, we should make this a Configurable class
 
 
+class NvChad:
+    @classmethod
+    def install(cls):
+        git_clone("https://github.com/NvChad/NvChad", "~/.config/nvim", depth=1)
+
+
+class LazyVim:
+    @classmethod
+    def install(cls):
+        cls._backup_config()
+        shell("git clone https://github.com/LazyVim/starter ~/.config/nvim")
+        shell("rm -rf ~/.config/nvim/.git")
+
+    @classmethod
+    def _backup_config(cls):
+        cmds = [
+            # required
+            "mv ~/.config/nvim ~/.config/nvim.bak",
+            # optional but recommended
+            "mv ~/.local/share/nvim ~/.local/share/nvim.bak",
+            "mv ~/.local/state/nvim ~/.local/state/nvim.bak",
+            "mv ~/.cache/nvim ~/.cache/nvim.bak",
+        ]
+        for cmd in cmds:
+            shell(cmd)
+
+
 def _download_latest_nvim():
-    cmd("sudo add-apt-repository -y ppa:neovim-ppa/stable")
-    cmd("sudo apt-get update && sudo apt-get install -y neovim")
-
-
-def _install_chadnvim():
-    # install chadnvim
-    git_clone("https://github.com/NvChad/NvChad", "~/.config/nvim", depth=1)
+    shell("sudo add-apt-repository -y ppa:neovim-ppa/stable")
+    shell("sudo apt-get update && sudo apt-get install -y neovim")
 
 
 def install():
     _download_latest_nvim()
-    _install_chadnvim()
+    NvChad.install()
